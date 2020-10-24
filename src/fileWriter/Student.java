0fe5bca7680dba6d94c8265ec3bc972739148e64
liamw <liamw@liams-air.lan>
2020-10-24 16:33:39 -0400

    First commit, class

---
+++ src/fileWriter/Student.java
@@ -0,0 +1,63 @@
+package fileWriter;
+
+public class Student {
+	
+    private String name;
+    private String id;
+    private char grade;
+    
+    
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public char getGrade() {
+		return grade;
+	}
+	public void setGrade(char grade) {
+		this.grade = grade;
+		
+	}
+	
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Student other = (Student) obj;
+		if (grade != other.grade)
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Student [name=" + name + ", id=" + id + ", grade=" + grade + "]";
+	}
+    
+	
+	
+    
+
+}
